7a41b7aeafafa73ec224905e6707d7afffcb42c3
Abhi Basu Thakur <abhibasuthakur@gmail.com>
2020-04-25 18:02:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+@FunctionalInterface
+interface MyFunctionalInterface {
+
+	//A method with no parameter
+    public String sayHello();
+}
+
+@FunctionalInterface
+interface MyFunctionalInterface2 {
+    public int Increment(int a);
+}
+public class Main {
+
+   public static void main(String args[]) {
+        // lambda expression
+    	MyFunctionalInterface msg = () -> {
+    		return "Hello";
+    	};
+
+      MyFunctionalInterface2 numBer = (num) -> num+5;
+      System.out.println(msg.sayHello());
+      System.out.println(numBer.Increment(2));
+    }
+}
